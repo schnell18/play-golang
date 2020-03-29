d2a29eb64ef4c5763e36a1d6a41cbb06e730095a
Justin Zhang <schnell18@gmail.com>
2020-03-29 20:41:35 +0800

    Finish excercise 5.1

--- recursion/findlinks.go
+++ recursion/findlinks.go
@@ -19,15 +19,19 @@ func main() {
 }
 
 func visit(links []string, n *html.Node) []string {
-	if n.Type == html.ElementNode && n.Data == "a" {
-		for _, a := range n.Attr {
-			if a.Key == "href" {
-				links = append(links, a.Val)
+	if n != nil {
+		if n.Type == html.ElementNode && n.Data == "a" {
+			for _, a := range n.Attr {
+				if a.Key == "href" {
+					links = append(links, a.Val)
+				}
 			}
 		}
-	}
-	for c := n.FirstChild; c != nil; c = c.NextSibling {
-		links = visit(links, c)
+		// for c := n.FirstChild; c != nil; c = c.NextSibling {
+		// 	links = visit(links, c)
+		// }
+		links = visit(links, n.FirstChild)
+		links = visit(links, n.NextSibling)
 	}
 	return links
 }
