29395e55c758a7598c2dc8ece5f8dda28aa4ebe7
Justin Zhang <schnell18@gmail.com>
2023-05-17 00:18:03 +0800

    Add fuzz test for fib

--- ch2/fib/fibonacci_test.go
+++ ch2/fib/fibonacci_test.go
@@ -1,9 +1,12 @@
 package fib
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestFibonacci(t *testing.T) {
-
 	expected := []struct {
 		N int
 		F int
@@ -29,4 +32,22 @@ func TestFibonacci(t *testing.T) {
 			t.Errorf("%dth test case fibonacci(%d) == %d != %d", i+1, exp.N, actF, exp.F)
 		}
 	}
+}
+
+func FuzzTestFibonacci(f *testing.F) {
+	testcases := []int{1, 2, 4, 5, 10, 13}
+	for _, tc := range testcases {
+		f.Add(tc)
+	}
+	f.Fuzz(func(t *testing.T, n int) {
+		assert := assert.New(t)
+		ret := Fibonacci(n)
+		if n == 0 {
+			assert.Equal(0, ret)
+		} else if n == 1 || n == 2 {
+			assert.Equal(1, ret)
+		} else {
+			assert.Equal(ret, Fibonacci(n-1)+Fibonacci(n-2))
+		}
+	})
 }
