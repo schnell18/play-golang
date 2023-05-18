f2536e77d6695fabc6e829fd3aa24ba3507b33bb
Justin Zhang <schnell18@gmail.com>
2023-05-18 13:47:58 +0800

    Channel and loop variable capture

---
+++ intv1/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	ch := make(chan *int, 4)
+	array := []int{1, 2, 3, 4}
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		for _, value := range array {
+			// value := value
+			ch <- &value
+		}
+		// close(ch)
+	}()
+	go func() {
+		for value := range ch {
+			fmt.Println(*value) // what will be printed here?
+		}
+		wg.Done()
+	}()
+
+	wg.Wait()
+}
